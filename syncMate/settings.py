cb322a754aa08d1ec528face4a2d44a9f7afebdc
avinashbakshi01 <avinash@wesecureapp.com>
2023-11-19 12:49:49 +0530

    updated allowed host

--- syncMate/settings.py
+++ syncMate/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y)+cp(*7_8c1#%^d#rq_&3270&459v)vxfvzh36+(q@58je(y7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
